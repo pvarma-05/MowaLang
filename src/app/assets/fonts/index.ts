458cd5263da2126871d0ffc111d07e7256fcd36c
Pradeep Varma <pradeeppenumatsa555@gmail.com>
2025-05-16 21:41:29 +0530

    Removed Old Files

--- src/app/assets/fonts/index.ts
+++ src/app/assets/fonts/index.ts
@@ -1,15 +1,7 @@
 import localFont from "next/font/local";
 
-export const myfont=localFont({
-    src:'./CoffeeTown.otf',
-    variable : '--font-gw'
-})
 
-export const myfont2=localFont({
+export const myfont=localFont({
     src:'./blowbrush.ttf',
     variable: '--font-wg'
-})
-export const myfont3=localFont({
-    src:'./Fonarto.ttf',
-    variable: '--font-ft'
 })
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,15 @@
 import './global.css';
 import { RootProvider } from 'fumadocs-ui/provider';
-import { Outfit,Poppins,Fira_Code } from 'next/font/google';
+import { Outfit,Fira_Code } from 'next/font/google';
 import type { ReactNode } from 'react';
-import { myfont,myfont2,myfont3 } from "./assets/fonts";
+import { myfont } from "./assets/fonts";
 
 const outfit = Outfit({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   subsets: ["latin"],
   variable: "--font-outfit",
 });
-const ak = Poppins({
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  subsets: ["latin"],
-  variable: "--font-at",
-});
+
 const firaCode = Fira_Code({
   weight: ["400", "500", "700"],
   subsets: ["latin"],
@@ -22,7 +18,7 @@ const firaCode = Fira_Code({
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en" className={`${outfit.variable} ${myfont.variable} ${myfont2.variable} ${myfont3.variable} ${ak.variable} ${firaCode.variable} antialiased`} suppressHydrationWarning>
+    <html lang="en" className={`${outfit.variable} ${myfont.variable}   ${firaCode.variable} antialiased`} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen scrollbar-thin scrollbar-thumb-[#263238] scrollbar-track-[#001220] ">
         <RootProvider theme={{enabled:false}}>{children}</RootProvider>
       </body>
