916e50c18f860af17eecf98f5065229522cc77df
Pradeep Varma <pradeeppenumatsa555@gmail.com>
2025-05-26 18:06:16 +0530

    Added analytics

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Outfit, Fira_Code } from 'next/font/google';
 import type { ReactNode } from 'react';
 import { myfont } from "./assets/fonts"
 import '@/app/monaco-config'
+import { Analytics } from "@vercel/analytics/next"
 
 const outfit = Outfit({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -22,7 +23,8 @@ export default function Layout({ children }: { children: ReactNode }) {
     <html lang="en" className={`${outfit.variable} ${myfont.variable}   ${firaCode.variable} antialiased scroll-smooth`} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen scrollbar-thin scrollbar-thumb-[#263238] scrollbar-track-[#001220] selection:bg-red-200/35 selection:text-[#A93E39] dark:selection:bg-red-400/8">
         <RootProvider>
-            {children}
+          {children}
+          <Analytics />
         </RootProvider>
       </body>
     </html>
